c24a7530a2c376c00482e9d3db7ab52cdca5be3f
Hinata-Sekiguchi <49296310+Hinata-Sekiguchi@users.noreply.github.com>
2020-09-12 09:46:48 +0900

    Update read.php

--- 20200606/public/bbs/read.php
+++ 20200606/public/bbs/read.php
@@ -1,8 +1,7 @@
 <?php
-// データベースハンドラ作成 db名/ユーザー名/パスワードを独自のものに設定している場合は書き換えてください。
+// データベースハンドラの作成
 $dbh = new PDO('mysql:host=mysql;dbname=2020techc_db', '2020techc_username', '2020techc_password');
 
-// 全行取得 id降順(新しい投稿が上にくる)
 $select_sth = $dbh->prepare('SELECT name, body, created_at FROM bbs_entries ORDER BY id DESC');
 $select_sth->execute();
 $rows = $select_sth->fetchAll();
@@ -13,10 +12,7 @@ $rows = $select_sth->fetchAll();
 </head>
 <body>
   <h1>掲示板</h1>
-  <!--
-    投稿用フォーム
-    読み込みページに投稿用フォームを用意してあげると利用者は便利です。
-  -->
+  <!-- 投稿用フォーム　-->
   <?php if(!empty($_COOKIE["login_id"])): ?>
   <form method="POST" action="./write.php" style="margin: 2em;">
     <div>
@@ -33,12 +29,7 @@ $rows = $select_sth->fetchAll();
   <!-- 投稿用フォームここまで -->
   <hr>
 
-  <!--
-    投稿の表示
-    foreachを用いたループで表現しています。
-    利用者が任意の内容を投稿する部分(名前と本文)は htmlspecialchars() を用いエスケープします。
-      XSS対策です。
-  -->
+  <!-- 投稿の表示 -->
   <?php foreach ($rows as $row) : ?>
   <div style="margin: 2em;">
     <span><?= htmlspecialchars($row['name']) ?>さんの投稿</span>
